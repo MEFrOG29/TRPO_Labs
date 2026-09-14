f259e240e47fcae3cde770acdc16607428a5bc0c
0109-09 <0109-09@softsol.local>
2026-09-14 09:56:03 +0300

    lab 7

---
+++ lab7/Lab7t2/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Diagnostics;
+
+var time = DateTime.Now;
+Stopwatch sw = new();
+sw.Start();
+var fileData = File.ReadAllLines("data.txt");
+Console.WriteLine($"Прочитано строк: {fileData.Length}");
+sw.Stop();
+TimeSpan ts = sw.Elapsed;
+Console.WriteLine($"[{time:yyyy-MM-dd HH:mm:ss}] Operation=ReadTxtFile, Elapsed={ts} ms");
---
+++ lab7/lab7/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.WriteLine("Введите первое число: ");
+string userValue1 = Console.ReadLine();
+Console.WriteLine("Введите второе число: ");
+string userValue2 = Console.ReadLine();
+
+try
+{
+    int num1 = Convert.ToInt32(userValue1);
+    int num2 = Convert.ToInt32(userValue2);
+
+    int sum = num1 + num2;
+    int dif = num1 - num2;
+    int mul = num1 * num2;
+    int div = num1 / num2;
+
+    Console.WriteLine($"{num1} + {num2} = {sum}");
+    Console.WriteLine($"{num1} - {num2} = {dif}");
+    Console.WriteLine($"{num1} * {num2} = {mul}");
+    Console.WriteLine($"{num1} / {num2} = {div}");
+}
+catch(FormatException ex)
+{
+    Console.WriteLine($"Ошибка, {ex.Message}");
+    var errorTime = DateTime.Now;
+    File.AppendAllText("log.txt", $"[{errorTime:yyyy-MM-dd HH:mm:ss}] {ex.GetType()} {ex.ToString()}");
+}
+catch(DivideByZeroException ex)
+{
+    Console.WriteLine($"Ошибка, {ex.Message}");
+    var errorTime = DateTime.Now;
+    File.AppendAllText("log.txt", $"[{errorTime:yyyy-MM-dd HH:mm:ss}] {ex.GetType()} {ex.ToString()}");
+}
+catch(OverflowException ex)
+{
+    Console.WriteLine($"Ошибка, {ex.Message}");
+    var errorTime = DateTime.Now;
+    File.AppendAllText("log.txt", $"[{errorTime:yyyy-MM-dd HH:mm:ss}] {ex.GetType()} {ex.ToString()}");
+}

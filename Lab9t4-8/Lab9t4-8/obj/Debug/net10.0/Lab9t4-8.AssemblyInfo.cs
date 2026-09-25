23f59bd3d340b40e17e1e5f70c41019fc495e9e3
0109-09 <0109-09@softsol.local>
2026-09-25 11:02:42 +0300

    lab10 complete t1-t6

--- Lab9t4-8/Lab9t4-8/obj/Debug/net10.0/Lab9t4-8.AssemblyInfo.cs
+++ Lab9t4-8/Lab9t4-8/obj/Debug/net10.0/Lab9t4-8.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab9t4-8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e064b47a521ec988c4fe5b6581028d9d3fe6a7cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e56a2c957837034f93e55168ebd6c1b96567d99")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab9t4-8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab9t4-8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
